34d503bc9c762d91272707e7e48fd8e545ad36cf
王珍洋 <zhenyang.wang@melot.cn>
2026-08-04 15:04:37 +0800

    #

---
+++ demo_1/demo1.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+flag=False
+if (flag):
+ print("123")
+ print("2")
+else:
+ print("456")
+var1=1
+var1=2
+print(var1)
+import langchain
+print(langchain.__version__)
---
+++ demo_1/model_init.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+from langchain_deepseek import ChatDeepSeek
+load_dotenv(override=True)
+dsApiKey=os.getenv("ds_api_key")
+dsApiUrl=os.getenv("ds_base_url")
+deepseekClient=ChatDeepSeek(
+     model="deepseek-v4-flash",
+     api_key=dsApiKey,
+     base_url=dsApiUrl
+)
+response=deepseekClient.invoke("你是谁")
+print(response)
